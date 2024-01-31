166863e5e26095797b185f5f7087f56ace67b2a4
moljac <mcvjetko@holisticware.net>
2024-01-31 11:34:50 +0100

    javier's sample with wrapper around IHostApplicationBuilder

---
+++ samples/javier/20240130/ClientAppsIntegration.MAUI/ClientAppsIntegration.MAUI/WrapperMauiAppBuilder.cs
@@ -0,0 +1,52 @@
+﻿#nullable disable
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.Metrics;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ClientAppsIntegration.MAUI
+{
+    public class WrapperMauiAppBuilder : IHostApplicationBuilder
+    {
+        readonly MauiAppBuilder _appBuilder;
+
+        public WrapperMauiAppBuilder(MauiAppBuilder appBuilder)
+        {
+            _appBuilder = appBuilder;
+        }
+
+        public IConfigurationManager Configuration => _appBuilder.Configuration;
+
+        public IHostEnvironment Environment => new MauiHostEnvironment();
+
+        public ILoggingBuilder Logging => _appBuilder.Logging;
+
+        public IMetricsBuilder Metrics => null;
+
+        public IDictionary<object, object> Properties => throw new NotImplementedException();
+
+        public IServiceCollection Services => _appBuilder.Services;
+
+        public void ConfigureContainer<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory, Action<TContainerBuilder>? configure = null) where TContainerBuilder : notnull
+        {
+            _appBuilder.ConfigureContainer<TContainerBuilder>(factory, configure);
+        }
+
+        public MauiApp Build()
+        {
+            return _appBuilder.Build();
+        }
+    }
+
+    internal class MauiHostEnvironment : IHostEnvironment
+    {
+        public string ApplicationName { get; set; } = typeof(MauiHostEnvironment).Assembly.GetName().Name;
+
+        public IFileProvider ContentRootFileProvider { get; set; } = new PhysicalFileProvider(AppContext.BaseDirectory);
+        
+        public string ContentRootPath { get; set; } = AppContext.BaseDirectory;
+
+        public string EnvironmentName { get; set; } = "Development";
+    }
+}
