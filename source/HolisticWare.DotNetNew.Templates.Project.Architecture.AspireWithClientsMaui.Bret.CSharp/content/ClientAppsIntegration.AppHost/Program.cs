2a6f3d5b21422abcb4a6d13d304f45a29d685cbc
moljac <mcvjetko@holisticware.net>
2024-02-07 11:55:26 +0100

    template fixes

--- source/HolisticWare.DotNetNew.Templates.Project.Architecture.AspireWithClientsMaui.Bret.CSharp/content/ClientAppsIntegration.AppHost/Program.cs
+++ source/HolisticWare.DotNetNew.Templates.Project.Architecture.AspireWithClientsMaui.Bret.CSharp/content/ClientAppsIntegration.AppHost/Program.cs
@@ -5,11 +5,11 @@ var apiService = builder.AddProject<Projects.ClientAppsIntegration_ApiService>("
 // Register the client apps by project path as they target a TFM incompatible with the AppHost so can't be added as
 // regular project references (see the AppHost.csproj file for additional metadata added to the ProjectReference to
 // coordinate a build dependency though).
-builder.AddProject("winformsclient", "../ClientAppsIntegration.WinForms/ClientAppsIntegration.WinForms.csproj")
-    .WithReference(apiService);
+// builder.AddProject("winformsclient", "../ClientAppsIntegration.WinForms/ClientAppsIntegration.WinForms.csproj")
+//     .WithReference(apiService);
 
-builder.AddProject("wpfclient", "../ClientAppsIntegration.WPF/ClientAppsIntegration.WPF.csproj")
-    .WithReference(apiService);
+// builder.AddProject("wpfclient", "../ClientAppsIntegration.WPF/ClientAppsIntegration.WPF.csproj")
+//     .WithReference(apiService);
 
 builder.AddMauiProject("mauiclient", "../ClientAppsIntegration.MAUI")
    .WithReference(apiService);
